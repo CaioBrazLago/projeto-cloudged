aeab5339cc80ef87207655ff41eae5d99165e973
Caio Braz <caioanizer@gmail.com>
2026-08-19 13:34:19 -0300

    refactor(notas): change Nota class properties to private and add getter methods

--- src/modules/notas/entities/nota.entity.ts
+++ src/modules/notas/entities/nota.entity.ts
@@ -2,12 +2,47 @@ import { NotaItem } from './nota-item.entity';
 
 export class Nota {
   constructor(
-    public readonly id: string,
-    public readonly numeroNota: string,
-    public readonly uf: string,
-    public readonly dataEmissao: Date,
-    public readonly creditoTotal: number,
-    public readonly payloadHash: string,
-    public readonly itens: NotaItem[],
+    private readonly _id: string | null,
+    private readonly _numeroNota: string,
+    private readonly _uf: string,
+    private readonly _dataEmissao: Date,
+    private _creditoTotal: number,
+    private readonly _payloadHash: string,
+    private readonly _itens: NotaItem[],
   ) {}
+
+  get id(): string | null {
+    return this._id;
+  }
+
+  get numeroNota(): string {
+    return this._numeroNota;
+  }
+
+  get uf(): string {
+    return this._uf;
+  }
+
+  get dataEmissao(): Date {
+    return this._dataEmissao;
+  }
+
+  get creditoTotal(): number {
+    return this._creditoTotal;
+  }
+
+  get payloadHash(): string {
+    return this._payloadHash;
+  }
+
+  get itens(): ReadonlyArray<NotaItem> {
+    return this._itens;
+  }
+
+  calcularCreditoTotal(): void {
+    this._creditoTotal = this._itens.reduce(
+      (total, item) => total + (item.credito ?? 0),
+      0,
+    );
+  }
 }
