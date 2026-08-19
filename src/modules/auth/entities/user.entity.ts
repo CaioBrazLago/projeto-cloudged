281dc83afd47a79795a18e4752ecefa5ee97ba3b
Caio Braz <caioanizer@gmail.com>
2026-08-19 19:20:15 -0300

    refactor(auth): remove user-related data, entity, and role enum

--- src/modules/auth/entities/user.entity.ts
+++
@@ -1,9 +0,0 @@
-import { UserRole } from '../enums/user-role.enum';
-
-export class User {
-  constructor(
-    public readonly usuario: string,
-    public readonly passwordHash: string,
-    public readonly role: UserRole,
-  ) {}
-}
--- src/modules/auth/enums/user-role.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum UserRole {
-  OPERADOR = 'operador',
-  AUDITOR = 'auditor',
-}
