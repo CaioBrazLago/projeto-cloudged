b2620a2d5102e820289ccdf3ca38fc88d797731c
Caio Braz <caioanizer@gmail.com>
2026-08-18 21:45:53 -0300

    feat(app): configure global ConfigModule and import PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+import { PrismaModule } from './database/prisma.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    PrismaModule,
+  ],
 })
 export class AppModule {}
