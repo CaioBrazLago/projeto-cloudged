7dc34a8ff03a51f46fa72a57512b9d05ad54e1fd
Caio Braz <caioanizer@gmail.com>
2026-08-19 13:39:51 -0300

    feat(aliquotas): initialize AliquotasModule with Prisma integration and resolver service

--- src/modules/aliquotas/aliquotas.module.ts
+++ src/modules/aliquotas/aliquotas.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { PrismaModule } from '../../database/prisma.module';
+import { AliquotaRepository } from './repositories/aliquota.repository';
+import { PrismaAliquotaRepository } from './repositories/prisma/prisma-aliquota.repository';
+import { AliquotaResolverService } from './services/aliquota-resolver.service';
+
+@Module({
+  imports: [PrismaModule],
+  providers: [
+    AliquotaResolverService,
+    {
+      provide: AliquotaRepository,
+      useClass: PrismaAliquotaRepository,
+    },
+  ],
+  exports: [AliquotaResolverService],
+})
 export class AliquotasModule {}
