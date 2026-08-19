fb0a7befa36968da823243fccf4016d0a4049d1f
Caio Braz <caioanizer@gmail.com>
2026-08-19 13:36:44 -0300

    refactor(notas): change NotaItem properties to private, add getter methods, and add methods to : calcularCredito marcarComoPendente

--- src/modules/notas/entities/nota-item.entity.ts
+++ src/modules/notas/entities/nota-item.entity.ts
@@ -1,13 +1,59 @@
-import { ItemStatus } from './item-status.enum';
+import Decimal from 'decimal.js';
+import { ItemStatus } from '../enums/item-status.enum';
 
 export class NotaItem {
   constructor(
-    public readonly id: string,
-    public readonly ncm: string,
-    public readonly quantidade: number,
-    public readonly valorUnitario: number,
-    public readonly aliquota: number | null,
-    public readonly credito: number | null,
-    public readonly status: ItemStatus,
+    private readonly _id: string | null,
+    private readonly _ncm: string,
+    private readonly _quantidade: number,
+    private readonly _valorUnitario: number,
+    private _aliquota: number | null,
+    private _credito: number | null,
+    private _status: ItemStatus,
   ) {}
+
+  get id(): string | null {
+    return this._id;
+  }
+
+  get ncm(): string {
+    return this._ncm;
+  }
+
+  get quantidade(): number {
+    return this._quantidade;
+  }
+
+  get valorUnitario(): number {
+    return this._valorUnitario;
+  }
+
+  get aliquota(): number | null {
+    return this._aliquota;
+  }
+
+  get credito(): number | null {
+    return this._credito;
+  }
+
+  get status(): ItemStatus {
+    return this._status;
+  }
+
+  calcularCredito(aliquota: number): void {
+    const credito = new Decimal(this._quantidade)
+      .mul(this._valorUnitario)
+      .mul(aliquota)
+      .toDecimalPlaces(2, Decimal.ROUND_HALF_UP);
+
+    this._aliquota = aliquota;
+    this._credito = credito.toNumber();
+    this._status = ItemStatus.CALCULADO;
+  }
+
+  marcarComoPendente(): void {
+    this._aliquota = null;
+    this._credito = null;
+    this._status = ItemStatus.PENDENTE_ALIQUOTA;
+  }
 }
