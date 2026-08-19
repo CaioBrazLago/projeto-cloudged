5672e360b8a2fb8d815d6e7c69e32fe93b05c553
Caio Braz <caioanizer@gmail.com>
2026-08-19 13:47:43 -0300

    feat(notas): initialize NotasModule with imports, controllers, and providers

--- src/modules/notas/notas.module.ts
+++ src/modules/notas/notas.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { PrismaModule } from '../../database/prisma.module';
+import { AliquotasModule } from '../aliquotas/aliquotas.module';
+import { NotasController } from './controllers/notas.controller';
+import { NotaRepository } from './repositories/nota.repository';
+import { PrismaNotaRepository } from './repositories/prisma/prisma-nota.repository';
+import { NotasService } from './services/notas.service';
+
+@Module({
+  imports: [PrismaModule, AliquotasModule],
+  controllers: [NotasController],
+  providers: [
+    NotasService,
+    {
+      provide: NotaRepository,
+      useClass: PrismaNotaRepository,
+    },
+  ],
+})
 export class NotasModule {}
