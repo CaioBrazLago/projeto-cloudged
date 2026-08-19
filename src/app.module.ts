f7346c04f2bc385c9f1f8bb53165366fb2cb1805
Caio Braz <caioanizer@gmail.com>
2026-08-19 13:48:01 -0300

    feat(app): add NotasModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { PrismaModule } from './database/prisma.module';
+import { NotasModule } from './modules/notas/notas.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { PrismaModule } from './database/prisma.module';
     }),
 
     PrismaModule,
+    NotasModule,
   ],
 })
 export class AppModule {}
