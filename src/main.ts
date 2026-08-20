d1fba2851ad43b4cd99b7dd19d51a1e8c498256d
Caio Braz <caioanizer@gmail.com>
2026-08-20 04:35:12 -0300

    feat(swagger): integrate Swagger for API documentation setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -13,6 +14,26 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('CloudGed - API ICMS-ST')
+    .setDescription(
+      'API para cálculo e consulta de créditos de ICMS-ST de notas fiscais',
+    )
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('docs', app, swaggerDocument);
 
   await app.listen(process.env.PORT ?? 3000);
 }
