3085b4df6192a6e471e15ff0e5fe68abfcde389e
Caio Braz <caioanizer@gmail.com>
2026-08-20 00:16:12 -0300

    feat(auth): configure JwtModule and update providers in AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import 'dotenv/config';
+
+import { UserModule } from '../user/user.module';
 import { AuthController } from './controllers/auth.controller';
+import { AuthGuard } from './guards/auth.guard';
+import { RolesGuard } from './guards/roles.guard';
 import { AuthService } from './services/auth.service';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1h',
+        algorithm: 'HS256',
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard, RolesGuard],
+  exports: [JwtModule, AuthGuard, RolesGuard],
 })
 export class AuthModule {}
