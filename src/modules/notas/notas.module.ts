418c2cb59c7de2e7d3071c256107413b9be38109
Caio Braz <caioanizer@gmail.com>
2026-08-20 00:16:46 -0300

    feat(notas): add AuthModule to NotasModule imports

--- src/modules/notas/notas.module.ts
+++ src/modules/notas/notas.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { PrismaModule } from '../../database/prisma.module';
 import { AliquotasModule } from '../aliquotas/aliquotas.module';
+import { AuthModule } from '../auth/auth.module';
 import { NotasController } from './controllers/notas.controller';
 import { NotaRepository } from './repositories/nota.repository';
 import { PrismaNotaRepository } from './repositories/prisma/prisma-nota.repository';
@@ -10,7 +11,7 @@ import { FindNotaService } from './services/find-nota.service';
 import { NotasService } from './services/notas.service';
 
 @Module({
-  imports: [PrismaModule, AliquotasModule],
+  imports: [PrismaModule, AliquotasModule, AuthModule],
   controllers: [NotasController],
   providers: [
     NotasService,
