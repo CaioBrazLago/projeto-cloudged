09e38dad8d61b39c7dc0b8b6c8df53248655fe74
Caio Braz <caioanizer@gmail.com>
2026-08-20 00:13:54 -0300

    feat(auth): add login endpoint to AuthController with DTO validation

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { LoginDto } from '../dto/login.dto';
+import type { AuthResponse } from '../services/auth.service';
 import { AuthService } from '../services/auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async Login(@Body() data: LoginDto): Promise<AuthResponse> {
+    const response = await this.authService.execute(data);
+
+    return response;
+  }
 }
